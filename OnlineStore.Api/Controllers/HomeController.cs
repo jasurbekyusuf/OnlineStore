231fc5ff4fe5d9b88a26e26952e4695e62b8b583
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-24 03:16:42 -0800

    INFRA: Initialize API Project

---
+++ OnlineStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace OnlineStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Online Store is running");
+    }
+}
