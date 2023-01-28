2ea55a284efcc01887a08e3ccbc6dc168c53725a
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-28 09:45:01 -0800

    Seller-> WIP

---
+++ OnlineStore.Infrastructure/Respositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using OnlineStore.Domain.Entities;
+using OnlineStore.Infrastructure.Interfeces;
+
+namespace OnlineStore.Infrastructure.Respositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OnlinestoreContext onlinestoreContext;
+
+
+        public ISellerRepository Sellers { get; private set; }
+
+        public ICustomerRepository Customers { get; private set; }
+
+        public IProductRepository Products { get; private set; }
+
+        public IOrderRepository Orders { get; private set; }
+
+        public IOrderProductRepository OrderProducts { get; private set; }
+
+        public UnitOfWork(OnlinestoreContext onlinestoreContext)
+        {
+            this.onlinestoreContext = onlinestoreContext;
+
+            Sellers = new SellerRepository(onlinestoreContext);
+            Customers = new CustomerRepository(onlinestoreContext);
+            Products = new ProductRepository(onlinestoreContext);
+            Orders = new OrderRepository(onlinestoreContext);
+            OrderProducts = new OrderProductRepository(onlinestoreContext);
+        }
+
+        public void Dispose()
+        {
+            onlinestoreContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await onlinestoreContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ OnlineStore.Service/DTOs/Seller/SellerForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Service.DTOs.Seller
+{
+    public class SellerForCreationDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
