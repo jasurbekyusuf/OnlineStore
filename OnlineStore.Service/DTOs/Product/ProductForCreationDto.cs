b6174b9f4848436531cc49acec13d8d2059d17d5
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-29 10:45:21 -0800

    DTOs: Add Product

---
+++ OnlineStore.Service/DTOs/Product/ProductForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore.Service.DTOs.Product
+{
+    public class ProductForCreationDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
