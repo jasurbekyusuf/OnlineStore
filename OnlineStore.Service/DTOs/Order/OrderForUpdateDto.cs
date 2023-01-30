67be6494e5a08ed755e38909563536fbb263f3c3
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-01-30 03:56:32 -0800

    DTOs: Add OrderForUpdate

---
+++ OnlineStore.Service/DTOs/Order/OrderForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using CoworkingBooking.Domain.Enums;
+
+namespace CoworkingBooking.Service.DTOs.Order
+{
+    public class OrderForUpdateDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+    }
+}
