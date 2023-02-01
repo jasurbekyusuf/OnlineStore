1a1184bd3441de48f0389e1d71f72753d26e9a62
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-02-01 17:42:08 +0500

    CODE RUB: Clean Up

--- OnlineStore.Service/DTOs/Order/OrderForCreationDto.cs
+++ OnlineStore.Service/DTOs/Order/OrderForCreationDto.cs
@@ -2,9 +2,10 @@
 {
     public class OrderForCreationDto
     {
-        public int? ProductId { get; set; }
-        public int? CustomerId { get; set; }
-        public int Count { get; set; }
+        public int ProductId { get; set; }
+        public int SellerId { get; set; }
+        public int CustomerId { get; set; }
+        public int Quantities { get; set; }
         public DateTimeOffset? CreatedDate { get; set; }
         public DateTimeOffset? UpdatedDate { get; set; }
     }
